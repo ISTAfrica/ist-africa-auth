cb8181339f6ba87eaf74d196af354f250a3e9eb2
Ardinenukuri <a.nukuri@alustudent.com>
2025-10-17 14:21:53 +0200

    adding other necessary directory

--- IAA-Frontend/IAA/src/app/test/page.tsx
+++ IAA-Frontend/IAA/src/app/test/page.tsx
@@ -7,8 +7,8 @@ interface TestData {
 
 async function getTestData(): Promise<TestData> {
 
-  const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL 
-    ? `https://${process.env.NEXT_PUBLIC_API_BASE_URL}` 
+  const apiBaseUrl = process.env.VERCEL_URL 
+    ? `https://${process.env.VERCEL_URL}` 
     : 'http://localhost:3000';
 
   const response = await fetch(`${apiBaseUrl}/api/test`, {
