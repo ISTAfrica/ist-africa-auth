bc9b95bee1977ca33d3a91d368978e11b70b7649
Sophie Mukamugema <sofidele12@.com>
2025-10-20 18:05:15 +0200

    deleting dist

--- dist/models/users/entities/user.entity.d.ts
+++
@@ -1,8 +0,0 @@
-import { Model } from 'sequelize-typescript';
-export declare class User extends Model {
-    id: number;
-    email: string;
-    name: string;
-    password: string;
-    isActive: boolean;
-}
