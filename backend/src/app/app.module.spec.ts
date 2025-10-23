52209caae82ae8aa428f390157ddb916fa6f9184
Sophie Mukamugema <sofidele12@.com>
2025-10-23 22:06:25 +0200

    working on test

--- backend/src/app/app.module.spec.ts
+++ backend/src/app/app.module.spec.ts
@@ -1,44 +1,44 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { AppModule } from './app.module';
-// import { ConfigModule } from '@nestjs/config';
-// import { SequelizeModule } from '@nestjs/sequelize';
-// import { UsersModule } from '../models/users/users.module';
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from './app.module';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { UsersModule } from '../models/users/users.module';
 
-// describe('AppModule', () => {
-//   let appModule: TestingModule;
+describe('AppModule', () => {
+  let appModule: TestingModule;
 
-//   beforeEach(async () => {
-//     const moduleBuilder = await Test.createTestingModule({
-//       imports: [AppModule],
-//     })
-//       .overrideModule(ConfigModule)
-//       .useModule(
-//         ConfigModule.forRoot({
-//           isGlobal: true,
-//           ignoreEnvFile: true,
-//           load: [() => ({ TEST_VAR: 'mocked' })],
-//         }),
-//       )
-//       .overrideModule(SequelizeModule)
-//       .useModule(
-//         SequelizeModule.forRoot({
-//           dialect: 'sqlite',
-//           storage: ':memory:',
-//         }),
-//       )
+  beforeEach(async () => {
+    const moduleBuilder = await Test.createTestingModule({
+      imports: [AppModule],
+    })
+      .overrideModule(ConfigModule)
+      .useModule(
+        ConfigModule.forRoot({
+          isGlobal: true,
+          ignoreEnvFile: true,
+          load: [() => ({ TEST_VAR: 'mocked' })],
+        }),
+      )
+      .overrideModule(SequelizeModule)
+      .useModule(
+        SequelizeModule.forRoot({
+          dialect: 'sqlite',
+          storage: ':memory:',
+        }),
+      )
 
-//       .compile();
+      .compile();
 
-//     appModule = moduleBuilder;
-//   });
-//   it('should be defined and compile successfully with mocked dependencies', () => {
-//     expect(appModule).toBeDefined();
-//   });
-//   it('should include the UsersModule', () => {
-//     const hasUsersModule = appModule.get(UsersModule);
-//     expect(hasUsersModule).toBeDefined();
-//   });
-//   afterAll(async () => {
-//     await appModule.close();
-//   });
-// });
+    appModule = moduleBuilder;
+  });
+  it('should be defined and compile successfully with mocked dependencies', () => {
+    expect(appModule).toBeDefined();
+  });
+  it('should include the UsersModule', () => {
+    const hasUsersModule = appModule.get(UsersModule);
+    expect(hasUsersModule).toBeDefined();
+  });
+  afterAll(async () => {
+    await appModule.close();
+  });
+});
