0069d3db2728a9dfe829e8495a5c325f506d08cf
Sophie Mukamugema <sofidele12@.com>
2025-10-29 15:49:03 +0200

    working on test

--- backend/src/config/database.config.ts
+++ backend/src/config/database.config.ts
@@ -3,7 +3,7 @@ import { SequelizeModuleOptions } from '@nestjs/sequelize';
 export const databaseConfig = (): SequelizeModuleOptions => ({
   dialect: 'postgres',
   host: process.env.DB_HOST ?? 'localhost',
-  port: Number(process.env.DB_PORT) || 5433,
+  port: Number(process.env.DB_PORT) || 5432,
   username: process.env.DB_USERNAME ?? 'postgres',
   password: process.env.DB_PASSWORD ?? 'postgres',
   database: process.env.DB_DATABASE ?? 'IAA',
