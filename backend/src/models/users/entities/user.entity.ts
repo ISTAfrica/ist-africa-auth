7493e4409a2d3137bfe44838dd6a8581fb5dd746
Afua Hamissi <lyhamissi@gmail.com>
2025-11-10 16:13:35 +0200

    removed migration and activated membership status

--- backend/src/models/users/entities/user.entity.ts
+++ backend/src/models/users/entities/user.entity.ts
@@ -68,12 +68,12 @@ export class User extends Model {
   })
   declare avatarUrl: string | null;
 
-  // @Column({
-  //   type: DataType.ENUM('ist_member', 'ext_member'),
-  //   allowNull: false,
-  //   defaultValue: 'ext_member',
-  // })
-  // declare membershipStatus: 'ist_member' | 'ext_member';
+  @Column({
+    type: DataType.ENUM('ist_member', 'ext_member'),
+    allowNull: false,
+    defaultValue: 'ext_member',
+  })
+  declare membershipStatus: 'ist_member' | 'ext_member';
 
   @Column({
     type: DataType.ENUM('user', 'admin'),
