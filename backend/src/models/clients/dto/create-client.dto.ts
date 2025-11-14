02e6c68da4c213d39596aebfc0b73b44413d7284
Sophie Mukamugema <sofidele12@.com>
2025-11-14 05:18:40 +0200

    Client App crud operations

--- backend/src/models/clients/dto/create-client.dto.ts
+++ backend/src/models/clients/dto/create-client.dto.ts
@@ -29,13 +29,6 @@ export class CreateClientDto {
   @IsOptional()
   description?: string;
 
-<<<<<<< HEAD
-  @ApiProperty({ example: 'http://localhost:3001/callback', description: 'The secure HTTP callback URL for the OAuth2 flow.' })
-  @IsUrl({ require_protocol: true, protocols: ['http', 'https'], require_tld: false, }) 
-  redirect_uri: string;
-
-  @ApiProperty({ example: ['http://localhost:3001'], description: 'An array of allowed origins for CORS.' })
-=======
   @ApiProperty({
     example: 'http://academy.ist.africa/callback',
     description: 'The secure HTTP callback URL for the OAuth2 flow.',
@@ -48,14 +41,16 @@ export class CreateClientDto {
     example: ['https://academy.ist.africa', 'https://app.academy.ist.africa'],
     description: 'An array of allowed origins for CORS.',
   })
->>>>>>> e6be49cb (Client App crud operations)
   @IsArray()
   @ArrayNotEmpty()
   @ArrayUnique()
-  @IsUrl({ 
-    require_protocol: true,
-    protocols: ['http', 'https'],
-    require_tld: false,
-  }, { each: true })
+  @IsUrl(
+    {
+      require_protocol: true,
+      protocols: ['http', 'https'],
+      require_tld: false,
+    },
+    { each: true },
+  )
   allowed_origins: string[];
 }
