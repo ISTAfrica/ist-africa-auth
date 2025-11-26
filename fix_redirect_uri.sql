7fd4d9b972c8737a15294432804ff39a21949d28
Afuwa Hamissi <167885060+lyhamissi@users.noreply.github.com>
2025-11-26 11:51:55 +0200

    deleted fix file

--- fix_redirect_uri.sql
+++ fix_redirect_uri.sql
@@ -1,12 +1,1 @@
--- Fix redirect URI mismatch for client
--- Update the redirect_uri to match what your client app is sending
-
-UPDATE clients
-SET redirect_uri = 'http://localhost:3001/api/callback'
-WHERE client_id = 'c30b03b35a608c161d041bacf4771bf3';
-
--- Verify the update
-SELECT client_id, name, redirect_uri, status 
-FROM clients 
-WHERE client_id = 'c30b03b35a608c161d041bacf4771bf3';
 
