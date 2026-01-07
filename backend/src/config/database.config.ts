e6419cedc83425ed2d9a69f4d0e7c490dcde9633
Afua Hamissi <lyhamissi@gmail.com>
2026-01-07 15:00:59 +0200

    adding deployed databse configurations

--- backend/src/config/database.config.ts
+++ backend/src/config/database.config.ts
@@ -6,12 +6,17 @@ export const databaseConfig = (): SequelizeModuleOptions => ({
   port: Number(process.env.DB_PORT) || 5432,
   username: process.env.DB_USERNAME ?? 'postgres',
   password: process.env.DB_PASSWORD ?? 'postgres',
-  database: process.env.DB_DATABASE ?? 'IAA',
+  database: process.env.DB_DATABASE ?? 'iaa_ndp2',
   autoLoadModels: true,
-  sync: { force: false }, // or `undefined` if you don't want auto-sync
+  sync: { force: false },
   logging: false,
-  timezone: '+00:00', // Force UTC timezone
+  timezone: '+00:00',
   dialectOptions: {
-    useUTC: true, // Use UTC for PostgreSQL
+    useUTC: true,
+
+    ssl: process.env.DB_HOST !== 'localhost' ? {
+      require: true,
+      rejectUnauthorized: false,
+    } : false,
   },
 });
